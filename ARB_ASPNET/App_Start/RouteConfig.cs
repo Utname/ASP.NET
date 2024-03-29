a1fb4eeedcd70287ded785352118d9282fac7e34
Utname <ut12thptdd@gmail.com>
2024-03-29 22:32:18 +0700

    Hi

--- ARB_ASPNET/App_Start/RouteConfig.cs
+++ ARB_ASPNET/App_Start/RouteConfig.cs
@@ -14,12 +14,6 @@ namespace ARB_ASPNET
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-               name: "About us",
-               url: "AboutUs",
-               defaults: new { controller = "Home", action = "AboutUs", id = UrlParameter.Optional }
-           );
-
-            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
--- ARB_ASPNET/Controllers/HomeController.cs
+++ ARB_ASPNET/Controllers/HomeController.cs
@@ -14,48 +14,8 @@ namespace ARB_ASPNET.Controllers
         // - viewbag: túi dữ liệu
         public ActionResult Index()
         {
-            ViewBag.title = "Welcome to my channel";
-            ViewBag.amount = 2000;
-            //Dữ liệu được public nhưng chỉ đuọc gọi và dùng 1 lần , xài được giữ các view
-            TempData["subTitle"] = "Say hi when you see me!!!";
             return View();
         }
-        public ActionResult AboutUs()
-        {
-            Data data = new Data();
-            data.name = "Trang gioi thieu";
-            data.description = "Trang gioi thieu cua toi";
-
-            Data data2 = new Data();
-            data2.name = "Trang gioi thieu2";
-            data2.description = "Trang gioi thieu cua toi2";
-
-            List<Data> list = new List<Data>();
-            list.Add(data);
-            list.Add(data2);
-            return View(list);
-        }
-
-        //View
-        public ActionResult Form()
-        {
-            Customer customer = new Customer();
-            customer.BirthDay = DateTime.Now;
-            customer.Gender = "Nam";
-            return View(customer);
-        }
-        //Submit
-        [HttpPost]
-        public ActionResult Form(Customer model)
-        {
-            ListCustomer.customers.Add(model);
-            return View(model);
-        }
         
-        // Hàm nhận form
-        public ActionResult FormNhanObject(Data model)
-        {
-            return View(model);  
-        }
     }
 }
