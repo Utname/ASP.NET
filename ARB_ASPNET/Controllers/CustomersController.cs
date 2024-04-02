27827af8dbb5828a3838c7dd5c9c1b3e26fdc595
Utname <ut12thptdd@gmail.com>
2024-04-02 23:28:03 +0700

    Data base

---
+++ ARB_ASPNET/Controllers/CustomersController.cs
@@ -0,0 +1,20 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ARB_ASPNET.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult getAllCustomer()
+        {
+            mapCustomers map = new mapCustomers();
+            var ds = map.getPageListCustomer(1,10);
+            return View(ds);
+        }
+    }
+}
---
+++ Data/mapCustomers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Entity;
+namespace Data
+{
+    public class mapCustomers
+    {
+        SuperMartketsEntities db = new SuperMartketsEntities();
+        public List<Customer> getAllCustomer()
+        {
+            return db.Customers.ToList();
+        }
+
+        public List<Customer> getPageListCustomer(int page,int size)
+        {
+            return db.Customers.ToList().Skip((page-1)*size).Take(size).ToList();
+        }
+
+        public Customer getDetailCustomer(int id)
+        {
+            return db.Customers.Find(id);
+        }
+    }
+}
